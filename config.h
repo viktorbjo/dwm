f82f1654a732ccb344956b0cbabd35ad65eda607
viktorbjo <viktor.bjorken@icloud.com>
2025-06-06 10:45:11 +0200

    newsboat

--- config.h
+++ config.h
@@ -97,7 +97,7 @@ static const Key keys[] = {
         { MODKEY,                       XK_a,  setgaps,            {.i = 0  } },
 	{ 0,            XF86XK_MonBrightnessUp,    spawn,          SHCMD(" brightnessctl set +10%")  },
 	{ 0,            XF86XK_MonBrightnessDown,  spawn,          SHCMD(" brightnessctl set 10%-") },
-	
+        { MODKEY|ShiftMask,             XK_n, spawn,               {.v = (const char*[]){ "st", "-e", "newsboat", NULL } } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
